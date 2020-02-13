c38c52595790728b285629e5e925b6c2f3786a4f
Yasir-Tariq <ynaseem78@gmail.com>
2020-02-13 15:23:36 -0800

    shared lib

---
+++ vars/sample.groovy
@@ -0,0 +1,23 @@
+pipeline {
+    agent any
+    stages {
+        stage('One') {
+                steps {
+                        echo 'This is stage one'
+                }
+        }
+	    stage('Two'){
+
+		steps {
+			input('Do you want to proceed?')
+      echo 'This is stage two'
+        }
+	    }
+      stage('Three'){
+
+    		steps {
+          echo 'This is stage Three'
+            }
+	    }
+    }
+}
